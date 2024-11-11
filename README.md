8654867ee3f07fb90302a81d0b7884601c06c1f3
yaacovdev <yaacov@purpl-ride.com>
2024-11-11 23:19:46 +0200

    add .gitignore, update README, and implement FastAPI structure with requirements

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # app_test
+
+```
+python -m venv venv
+
+.\venv\Scripts\activate
+
+pip install -r requirements.txt
+```
--- projet_test/main.py
+++ projet_test/main.py
@@ -1,1 +1,17 @@
-import math
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+
+router = APIRouter()
+
+
+@router.get("/")
+def hello():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    app.include_router(router)
+    import uvicorn
+
+    uvicorn.run(app)
