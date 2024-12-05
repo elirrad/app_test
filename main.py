927a364d2fe2c9919f0cf9b2290bf2fa9fc44350
elirrad <elimeleh.613@gmail.com>
2024-12-05 20:55:54 +0100

    a_DB

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+
 print("salut elimeleh le meilleur des petits freres")
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+
+#connexion a la DB
+import sqlite3
+connection = sqlite3.connect('DB.db')
+
+CREATE TABLE users (,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    age INT NOT NULL,
+);
+
+
+
+
+users=[{"name":"elimeleh","age":18},{"name":"eli","age":18}]
+
+
+@app.get("/")
+def hello(name):
+    return {"message": "Hello "+name}
+@app.get("/age")
+def get_age(age):
+    return{"age":age}
+@app.get("/user")
+def get_user(name):
+    for user in users:
+        if name==user["name"]:
+            return user
+
+    return{
+        "error":"not fond",
+    }
+
+
+if __name__ == "__main__":
+    app.include_router(app)
+    import uvicorn
+
+    uvicorn.run(app)
