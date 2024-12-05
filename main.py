c7d62fbf214eab46d4e34b5848e738ccec0c0a3e
elirrad <elimeleh.613@gmail.com>
2024-12-05 20:56:39 +0100

    add db

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 
 print("salut elimeleh le meilleur des petits freres")
 from fastapi import FastAPI, APIRouter
-
+import sqlalchemy
 app = FastAPI()
+#creation de table
+# CREATE TABLE userss (,
+#     nom TEXT NOT NULL,
+#     prenom TEXT NOT NULL,
+#     age INT NOT NULL,
+# );
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 #connexion a la DB
 import sqlite3
 connection = sqlite3.connect('DB.db')
 
-CREATE TABLE users (,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    age INT NOT NULL,
-);
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+
 
 
 
